5bde719f1b2ca5cbc31d158d274dba48c1e8c0a2
Sara Zori <sarazori@gmail.com>
2018-10-26 13:02:45 -0400

    Initial commit

---
+++ app/src/main/java/com/zoris/addressbook/LoginActivity2.java
@@ -0,0 +1,297 @@
+package com.zoris.addressbook;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.zoris.addressbook.data.DatabaseDescription;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity2 extends AppCompatActivity {
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private AutoCompleteTextView mUsernameView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+        // Set up the login form.
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.email);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.exit:
+                // Close application
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }*/
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid username, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password.
+        if (TextUtils.isEmpty(password)) {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(username)) {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameView;
+            cancel = true;
+        } else if (!isUsernameValid(username)) {
+            mUsernameView.setError(getString(R.string.error_invalid_email));
+            focusView = mUsernameView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            mAuthTask = new UserLoginTask(username, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isUsernameValid(String username) {
+        return username.length() > 1;
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mUsername;
+        private final String mPassword;
+        private String userid;
+
+        UserLoginTask(String username, String password) {
+            mUsername = username;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // Check for user in db. If existent then check password, else make account
+
+            // query for user
+            Cursor userCursor = LoginActivity2.this.getContentResolver().query(
+                    DatabaseDescription.User.CONTENT_URI, null,
+                    "username='"+mUsername+"'",
+                    null , null);
+
+            if ( userCursor.moveToFirst() ) {
+                userid = userCursor.getString(userCursor.getColumnIndex(DatabaseDescription.User._ID));
+                Log.d("Login", "Existent user. id: " + userid);
+
+                // Found user, check password
+                String password = userCursor.getString(userCursor.getColumnIndex(DatabaseDescription.User.COLUMN_PASSWORD));
+                userCursor.close();
+                if (mPassword.equals(password)) {
+                    return true; // success
+                } else {
+                    return false; // incorrect password
+                }
+            } else {
+                userCursor.close();
+
+                // User wasn't found, so register the new account here.
+                // create ContentValues object containing user's key-value pairs
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(DatabaseDescription.User.COLUMN_USERNAME, mUsername);
+                contentValues.put(DatabaseDescription.User.COLUMN_PASSWORD, mPassword);
+
+                // use Activity's ContentResolver to invoke
+                // insert on the AppContentProvider
+                Uri newUserUri = LoginActivity2.this.getContentResolver().insert(
+                        DatabaseDescription.User.CONTENT_URI, contentValues);
+                if (newUserUri == null) {
+                    return false; // failed somehow
+                } else {
+                    userid = newUserUri.getLastPathSegment();
+                    Log.d("Login", "New user. id: " + userid);
+                    return true;
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                finish();
+                Intent addressBookIntent = new Intent(LoginActivity2.this,MainActivity.class);
+                addressBookIntent.putExtra("user_id", userid);
+                LoginActivity2.this.startActivity(addressBookIntent);
+            } else {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/zoris/addressbook/data/AddressBookDatabaseHelper.java
@@ -0,0 +1,96 @@
+// AddressBookDatabaseHelper.java
+// SQLiteOpenHelper subclass that defines the app's database
+package com.zoris.addressbook.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.zoris.addressbook.data.DatabaseDescription.*;
+
+class AddressBookDatabaseHelper extends SQLiteOpenHelper {
+   private static final String DATABASE_NAME = "AddressBook.db";
+   private static final int DATABASE_VERSION = 1;
+
+   // constructor
+   public AddressBookDatabaseHelper(Context context) {
+      super(context, DATABASE_NAME, null, DATABASE_VERSION);
+      Log.d("DBHelper", "Contructor");
+      context.deleteDatabase(DATABASE_NAME);
+   }
+
+   // creates the contacts table when the database is created
+   @Override
+   public void onCreate(SQLiteDatabase db) {
+      // SQL for creating the contacts table
+      Log.d("DBHelper", "onCreate");
+
+      try {
+
+
+         final String CREATE_USERS_TABLE =
+                 "CREATE TABLE " + User.TABLE_NAME + "(" +
+                         User._ID + "integer primary key, " +
+                         User.COLUMN_USERNAME + "TEXT , " +
+                         User.COLUMN_PASSWORD + "TEXT);";
+         db.execSQL(CREATE_USERS_TABLE);//create user table
+//added part
+
+         Log.d("DBHelper", "Created table: " + User.TABLE_NAME);
+
+         // SQL for creating the contacts table
+         final String CREATE_CONTACTS_TABLE =
+                 "CREATE TABLE " + Contact.TABLE_NAME + "(" +
+                         Contact._ID + " integer primary key, " +
+                         Contact.COLUMN_NAME + " TEXT, " +
+                         Contact.COLUMN_PHONE + " TEXT, " +
+                         Contact.COLUMN_PHONE_MOBILE + " TEXT, " +
+                         Contact.COLUMN_PHONE_WORK + " TEXT, " +
+                         Contact.COLUMN_EMAIL + " TEXT, " +
+                         Contact.COLUMN_STREET + " TEXT, " +
+                         Contact.COLUMN_CITY + " TEXT, " +
+                         Contact.COLUMN_PROVINCE + " TEXT, " +
+                         Contact.COLUMN_PCODE + " TEXT, " +
+                         Contact.COLUMN_USER_ID + " integer, " +
+                         "FOREIGN KEY (" + Contact.COLUMN_USER_ID + ") " +
+                         "REFERENCES " + User.TABLE_NAME + "(_id));";
+         db.execSQL(CREATE_CONTACTS_TABLE); // create the contacts table
+
+         Log.d("DBHelper", "Created table: " + User.TABLE_NAME);
+
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+
+
+
+   }
+
+   // normally defines how to upgrade the database when the schema changes
+   @Override
+   public void onUpgrade(SQLiteDatabase db, int oldVersion,
+      int newVersion) {
+      db.execSQL("DROP TABLE IF EXISTS " + Contact.TABLE_NAME);
+      db.execSQL("DROP TABLE IF EXISTS " + User.TABLE_NAME);
+
+      onCreate(db);
+   }
+}
+
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ **************************************************************************/
